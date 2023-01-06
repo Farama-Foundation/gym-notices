cce76d2982209020d437f384b5f8421f5418e3a4
Jordan Terry <justinkterry@gmail.com>
2023-01-06 11:18:11 -0500

    Update notices.py

--- src/gym_notices/notices.py
+++ src/gym_notices/notices.py
@@ -10,7 +10,7 @@ notices = {
     "0.25.0": "",
     "0.25.1": "",
     "0.25.2": "",
-    "0.26.0": "",
-    "0.26.1": "",
-    "0.26.2": "",
+    "0.26.0": "All development of Gym has been moved to Gymnasium, a new package in the Farama Foundation that's maintained by the same team of developers who have maintained Gym for the past 18 months. Users of Gym v0.26.0 (your current version) can switch to the newest release of Gymnasium (v0.27.0) by simply replacing `import gym` with `import gymnasium as gym` with no additional changes. Gym is not planned to receive any future updates or bug fixes, and no further breaking changes will be made to the core API in Gymnasium. Users are encouraged to upgrade to Gymnasium at their earliest convenience.",
+    "0.26.1": "All development of Gym has been moved to Gymnasium, a new package in the Farama Foundation that's maintained by the same team of developers who have maintained Gym for the past 18 months. Users of Gym v0.26.1 (your current version) can switch to the newest release of Gymnasium (v0.27.0) by simply replacing `import gym` with `import gymnasium as gym` with no additional changes. Gym is not planned to receive any future updates or bug fixes, and no further breaking changes will be made to the core API in Gymnasium. Users are encouraged to upgrade to Gymnasium at their earliest convenience.",
+    "0.26.2": "All development of Gym has been moved to Gymnasium, a new package in the Farama Foundation that's maintained by the same team of developers who have maintained Gym for the past 18 months. Users of Gym v0.26.2 (your current version) can switch to the newest release of Gymnasium (v0.27.0) by simply replacing `import gym` with `import gymnasium as gym` with no additional changes. Gym is not planned to receive any future updates or bug fixes, and no further breaking changes will be made to the core API in Gymnasium. Users are encouraged to upgrade to Gymnasium at their earliest convenience.",
 }
